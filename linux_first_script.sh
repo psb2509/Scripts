80ceb3656959650c0bbf6f7d33d1263c237bc12f
Pradeep <pradeep.bisht0303@gmail.com>
2019-03-26 10:05:52 -0400

    Update linux_first_script.sh

--- linux_first_script.sh
+++ linux_first_script.sh
@@ -5,6 +5,7 @@ sudo yum upgrade -y                 # Upgrade all packages
 # This makes sure you’ve got GCC, make, git, etc. so you can build any modules that contain compiled code.
 sudo yum -y install @development    
 # Install Python 3
+cd /tmp
 curl -O https://www.python.org/ftp/python/3.4.10/Python-3.4.10.tgz
 tar -xvzf Python-3.4.10.tgz
  ./configure
