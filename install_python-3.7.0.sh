18a98afb9d7f20ed3cda3f1b75990343d6720afc
Pradeep <pradeep.bisht0303@gmail.com>
2019-03-26 10:58:00 -0400

    Update install_python-3.7.0.sh

--- install_python-3.7.0.sh
+++ install_python-3.7.0.sh
@@ -4,6 +4,7 @@ yum upgrade -y
 yum -y install @development 
 cd /tmp
 curl -O https://www.python.org/ftp/python/3.7.0/Python-3.7.0.tgz
+rm Python-3.7.0.tgz
 tar xf Python-3.7.0.tgz
 cd Python-3.7.0/
 
