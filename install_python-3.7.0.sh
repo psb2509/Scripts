eda4ad2128877ee627eeebf097252b9524693d0d
Pradeep <pradeep.bisht0303@gmail.com>
2019-03-26 10:35:57 -0400

    Create install_python-3.7.0.sh

---
+++ install_python-3.7.0.sh
@@ -0,0 +1,16 @@
+#!/bin/bash  
+# Download the source code of an official Python release.
+curl -O https://www.python.org/ftp/python/3.7.0/Python-3.7.0.tgz
+tar xf Python-3.7.0.tgz
+cd Python-3.7.0/
+# ./configure produces is another large, automatically generated script called Makefile .
+# The Makefile contains instructions on how to achive various tasks like “build” 
+# or “make the executable called python” or “run the test suite”.
+./configure
+
+make              # Compile the software 
+make test         # Test suite to make sure that it all works as expected:
+sudo make install # Install
+
+
+
