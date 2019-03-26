5468cebd7c32c989773a4db254f61ad6d8886a05
Pradeep <pradeep.bisht0303@gmail.com>
2019-03-26 17:25:31 -0400

    Update install_python-3.7.0.sh

--- install_python-3.7.0.sh
+++ install_python-3.7.0.sh
@@ -1,17 +1,2 @@
 #!/bin/bash 
-yum update -y
-yum upgrade -y
-yum -y install @development 
-cd /tmp
-curl -O https://www.python.org/ftp/python/3.7.0/Python-3.7.0.tgz
-rm Python-3.7.0.tgz
-tar xf Python-3.7.0.tgz
-cd Python-3.7.0/
-
-./configure
-make         
-# make test       
-make install
-
-
-
+sudo yum install -y python36.x86_64 python36-pip.noarch python36-setuptools.noarch python36-virtualenv.noarch
