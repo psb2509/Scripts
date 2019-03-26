e4fdae28ee46119fd20332ffc64383fbb2972913
Pradeep <pradeep.bisht0303@gmail.com>
2019-03-26 10:57:32 -0400

    Update install_python-3.7.0.sh

--- install_python-3.7.0.sh
+++ install_python-3.7.0.sh
@@ -1,10 +1,12 @@
 #!/bin/bash 
-# curl -O https://raw.githubusercontent.com/psb2509/Scripts/master/install_python-3.7.0.sh
+yum update -y
+yum upgrade -y
+yum -y install @development 
 cd /tmp
 curl -O https://www.python.org/ftp/python/3.7.0/Python-3.7.0.tgz
 tar xf Python-3.7.0.tgz
 cd Python-3.7.0/
-yum -y install @development 
+
 ./configure
 make         
 make test       
