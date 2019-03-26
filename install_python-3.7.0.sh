97cd79f6b029c46f9fe60165b06c9591aa511936
Pradeep <pradeep.bisht0303@gmail.com>
2019-03-26 17:27:18 -0400

    Update install_python-3.7.0.sh

--- install_python-3.7.0.sh
+++ install_python-3.7.0.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
-# yum list  |grep python36
+# yum list  |grep python36 # To list all python3.6 packages
 sudo yum install -y python36.x86_64 python36-pip.noarch python36-setuptools.noarch python36-virtualenv.noarch
 # python3 -m venv my-venv # To create virtual env
 
