3f4754b9b79bf54d3fb16009a17f9e92e585d1a5
Pradeep <pradeep.bisht0303@gmail.com>
2019-03-26 10:51:27 -0400

    Update install_python-3.7.0.sh

--- install_python-3.7.0.sh
+++ install_python-3.7.0.sh
@@ -1,4 +1,5 @@
-#!/bin/bash  
+#!/bin/bash 
+# curl -O https://raw.githubusercontent.com/psb2509/Scripts/master/install_python-3.7.0.sh
 cd /tmp
 curl -O https://www.python.org/ftp/python/3.7.0/Python-3.7.0.tgz
 tar xf Python-3.7.0.tgz
